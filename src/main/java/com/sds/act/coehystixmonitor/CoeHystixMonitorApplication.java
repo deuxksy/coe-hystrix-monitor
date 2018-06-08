ccb6cca9efa6707c0d33cd411f467afdda27c558
Marrakesh ACT <actmember@bas1-quebec13-70-30-181-71.dsl.bell.ca>
2018-06-08 11:57:01 +0900

    source

---
+++ src/main/java/com/sds/act/coehystixmonitor/CoeHystixMonitorApplication.java
@@ -0,0 +1,12 @@
+package com.sds.act.coehystixmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoeHystixMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoeHystixMonitorApplication.class, args);
+	}
+}
