0965d6ee26ada05acf62f42fbc5d241843bd7993
Marrakesh ACT <actmember@bas1-quebec13-70-30-181-71.dsl.bell.ca>
2018-06-08 15:10:18 +0900

    First Setup

--- src/main/java/com/sds/act/coehystixmonitor/CoeHystixMonitorApplication.java
+++ src/main/java/com/sds/act/coehystixmonitor/CoeHystixMonitorApplication.java
@@ -2,11 +2,13 @@ package com.sds.act.coehystixmonitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrixDashboard
 public class CoeHystixMonitorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CoeHystixMonitorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CoeHystixMonitorApplication.class, args);
+    }
 }
