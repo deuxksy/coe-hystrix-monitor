dcb4bc1b088247400156d321afb3e28f05160629
Marrakesh ACT <actmember@bas1-quebec13-70-30-181-71.dsl.bell.ca>
2018-06-15 18:29:35 +0900

    change all

--- src/main/java/com/sds/ces/hystix/dashboard/CoeHystixMonitorApplication.java
+++ src/main/java/com/sds/ces/hystix/dashboard/CoeHystixMonitorApplication.java
@@ -1,11 +1,17 @@
-package com.sds.act.coehystixmonitor;
+package com.sds.ces.hystix.dashboard;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableAutoConfiguration
 @EnableHystrixDashboard
+@EnableTurbine
 public class CoeHystixMonitorApplication {
 
     public static void main(String[] args) {
